2ee7d128a7834366d7147a265b32a5de225b820b
Bob Polacheck <bbb5483@yahoo.com>
2020-02-18 23:47:43 -0600

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { HeaderComponent } from './homepage/header/header.component';
+import { AboutMeComponent } from './homepage/about-me/about-me.component';
+import { ContactComponent } from './homepage/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    HeaderComponent,
+    AboutMeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
